b7cb300aecccb82ca38534c303152095eb9548d6
Nurali <nurali.shamel@icloud.com>
2024-03-01 22:42:09 +0600

    Initial Commit

---
+++ w math buster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  w math buster
+//
+//  Created by Nurali Shamel on 01.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
