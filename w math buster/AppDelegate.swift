f8257116951c579a6a5f65908e136dae22347c75
Nurali <nurali.shamel@icloud.com>
2024-03-04 19:03:59 +0500

    First commit

---
+++ w math buster/AppDelegate.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow()
+        window?.rootViewController = UINavigationController(rootViewController: ViewController2())
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
